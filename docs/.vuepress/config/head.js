246c5e6de57330e7375491020fc98b4efd490c56
ytppp <651003708@qq.com>
2021-06-22 11:05:45 +0800

    init

---
+++ docs/.vuepress/config/head.js
@@ -0,0 +1,12 @@
+module.exports = [
+  ['link', { rel: 'icon', href: '/img/favicon.ico' }],
+  [
+    'meta',
+    {
+      name: 'keywords',
+      content:
+        '个人博客,个人空间,前端开发,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,go,rust,css3,html5,Node,git,github,markdown,战舰,中国古诗词,历史,地理,时事,运动,动漫,WOW',
+    },
+  ],
+  ['meta', { name: 'theme-color', content: '#11a8cd' }],
+];
